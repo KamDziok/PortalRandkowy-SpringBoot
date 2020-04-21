1618c82c6ee4340aaa2294eb64ec4e5ff6fbf55d
KamDziok <kam_dziok@wp.pl>
2020-04-21 12:55:32 +0200

    Inicjalizacja repozyto poswieconemu SpringBoot.

---
+++ src/main/java/pl/lab2PPS/portalRandkowy/PortalRandkowyApplication.java
@@ -0,0 +1,13 @@
+package pl.lab2PPS.portalRandkowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRandkowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRandkowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lab2PPS/portalRandkowy/PortalRandkowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lab2PPS.portalRandkowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalRandkowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
