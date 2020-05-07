b61a30f896913fb0f10397eebeba2cb05eab4e18
KamDziok <kam_dziok@wp.pl>
2020-05-07 11:16:51 +0200

    Reset projektu

---
+++ src/main/java/pl/AILab1PPS/PortalRandkowy/PortalRandkowyApplication.java
@@ -0,0 +1,13 @@
+package pl.AILab1PPS.PortalRandkowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRandkowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRandkowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/AILab1PPS/PortalRandkowy/PortalRandkowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.AILab1PPS.PortalRandkowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalRandkowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
