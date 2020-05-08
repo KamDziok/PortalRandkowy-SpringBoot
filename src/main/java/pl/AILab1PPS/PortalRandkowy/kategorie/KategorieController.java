9c7201cfce7efbe318d040b19ca23b6c465f1a9d
KamDziok <kam_dziok@wp.pl>
2020-05-08 11:33:39 +0200

    Dodawanie i wyswietlanie Kategorii.

---
+++ src/main/java/pl/AILab1PPS/PortalRandkowy/kategorie/KategorieController.java
@@ -0,0 +1,25 @@
+package pl.AILab1PPS.PortalRandkowy.kategorie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("api/kategorie")
+@CrossOrigin
+public class KategorieController {
+
+    @Autowired
+    private KategorieRepository kategorieRepository;
+
+    @GetMapping
+    private ArrayList<Kategorie> getAllKategorie(){
+        return (ArrayList<Kategorie>) kategorieRepository.findAll();
+    }
+
+    @PostMapping
+    private Kategorie addKategorie(@RequestBody Kategorie kategorie){
+        return kategorieRepository.save(kategorie);
+    }
+}
---
+++ src/main/java/pl/AILab1PPS/PortalRandkowy/kategorie/KategorieRepository.java
@@ -0,0 +1,6 @@
+package pl.AILab1PPS.PortalRandkowy.kategorie;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface KategorieRepository extends PagingAndSortingRepository<Kategorie, Long> {
+}
