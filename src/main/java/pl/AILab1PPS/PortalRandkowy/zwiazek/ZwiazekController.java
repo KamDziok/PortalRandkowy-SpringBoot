8deed2ded19ed6cfb4446f0ac80d0d309538b2a9
KamDziok <kam_dziok@wp.pl>
2020-06-23 13:38:28 +0200

    Szukanie związków po id użytkowników.

--- src/main/java/pl/AILab1PPS/PortalRandkowy/zwiazek/ZwiazekController.java
+++ src/main/java/pl/AILab1PPS/PortalRandkowy/zwiazek/ZwiazekController.java
@@ -3,8 +3,11 @@ package pl.AILab1PPS.PortalRandkowy.zwiazek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.AILab1PPS.PortalRandkowy.statics.Relationship;
+import pl.AILab1PPS.PortalRandkowy.uzytkownik.Uzytkownik;
+import pl.AILab1PPS.PortalRandkowy.uzytkownik.UzytkownikRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/zwiazek")
@@ -12,10 +15,26 @@ import java.util.List;
 public class ZwiazekController {
     @Autowired
     private ZwiazekRepository zwiazekRepository;
+    @Autowired
+    private UzytkownikRepository uzytkownikRepository;
 
     @GetMapping
     private List<Zwiazek> getAllZwiazek(){
         return (List<Zwiazek>) zwiazekRepository.findAll();
+    }
+
+    @GetMapping("/uzytkownikA/{idA}/uzytkownikB/{idB}")
+    private Zwiazek getZwiazekUser(@PathVariable("idA") Long idUserA, @PathVariable("idB") Long idUserB){
+        Optional<Uzytkownik>userA = uzytkownikRepository.findById(idUserA);
+        Optional<Uzytkownik>userB = uzytkownikRepository.findById(idUserB);
+        Zwiazek result = null;
+        if(!userA.isEmpty() && !userB.isEmpty()){
+            result = zwiazekRepository.findByUzytkownikAAndUzytkownikB(userA.get(), userB.get());
+            if(result == null){
+                result = zwiazekRepository.findByUzytkownikAAndUzytkownikB(userB.get(), userA.get());
+            }
+        }
+        return result;
     }
 
     @PostMapping
